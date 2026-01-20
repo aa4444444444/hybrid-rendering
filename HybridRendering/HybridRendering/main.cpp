5629bab1fffcd00b4e2dce11c3ee94a4c0c069ce
AA <aadipranav@gmail.com>
2026-01-19 19:14:51 -0500

    Logging library works

--- HybridRendering/HybridRendering/main.cpp
+++ HybridRendering/HybridRendering/main.cpp
@@ -1,45 +1,82 @@
+#include <iostream>
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <iostream>
+#include <plog/Log.h>
+#include "plog/Appenders/ColorConsoleAppender.h"
+#include <plog/Formatters/TxtFormatter.h>
+#include "plog/Initializers/ConsoleInitializer.h"
+
+#include "shader.h"
+
+
+// forward declarations
+GLFWwindow* initializeWindow();
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-int main(){
-	using std::cout;
+int main() {
+    plog::ColorConsoleAppender<plog::TxtFormatter> consoleAppender;
+    plog::init(plog::debug, &consoleAppender);
 
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "HybridRendering", NULL, NULL);
-    if (window == NULL){
-        cout << "Failed to create GLFW window" << "\n";
-        glfwTerminate();
-        return -1;
+    PLOGD << "Calling initializeWindow()";
+    GLFWwindow* window = initializeWindow();
+    if (window == nullptr) {
+        PLOGE << "initializeWindow() failed";
     }
 
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        cout << "Failed to initialize GLAD" << "\n";
-        return -1;
-    }
-
-    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
-
+    PLOGD << "Entering render loop";
     while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
 	return 0;
+}
+
+GLFWwindow* initializeWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "HybridRendering", NULL, NULL);
+    if (window == NULL) {
+        PLOGE << "Failed to create GLFW window";
+        glfwTerminate();
+        return nullptr;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        PLOGE << "Failed to initialize GLAD";
+        return nullptr;
+    }
+
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+    return window;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
