5c16727a490be57c7a98330a3891ca715da810b2
AA <aadipranav@gmail.com>
2026-01-25 20:06:16 -0500

    Modularized code a bit more

---
+++ HybridRendering/HybridRendering/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace Constants {
+	inline constexpr unsigned int SCR_WIDTH{ 1920 };
+	inline constexpr unsigned int SCR_HEIGHT{ 1080 };
+	inline constexpr unsigned int NR_LIGHTS{ 32 };
+}
+
+#endif // !CONSTANTS_H
---
+++ HybridRendering/HybridRendering/utility.h
@@ -0,0 +1,26 @@
+#ifndef UTILITY_H
+#define UTILITY_H
+
+#include <string_view>
+
+#include <GLFW/glfw3.h>
+
+#include "settings.h"
+
+namespace Utility {
+	GLFWwindow* initializeWindow();
+
+	void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+	void processInput(GLFWwindow* window, Settings::RenderSettings& renderSettings, float deltaTime);
+
+	unsigned int loadTexture(std::string_view path, int activeTextureUnit);
+
+	void renderCube();
+
+	void renderQuad();
+
+	void setupImguiWindow(Settings::RenderSettings& renderSettings);
+}
+
+#endif // !UTILITY_H
